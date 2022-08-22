0c36d0d6de9b60b68e71f94dbae4528eeee960e7
Cristian Mannella <cristianmannellapc@gmail.com>
2022-08-22 12:25:40 +0200

    RamService doc and unit test

--- src/Service/Computer/RamService.php
+++ src/Service/Computer/RamService.php
@@ -16,14 +16,27 @@
             $this->ramRepository = $ramRepository;
         }
 
-        public function insert(Ram $s):void{
-            $ram = $this->ramRepository->selectById($s->RamID);
-            if($ram->ModelName == $s->ModelName && $ram->Size == $s->Size)
+        /**
+         * Insert ram
+         * @param Ram $r The ram to insert
+         * @return Ram The ram inserted
+         * @throws ServiceException If the same ram is already inserted
+         * @throws RepositoryException If the insert fails
+         */
+        public function insert(Ram $r):Ram{
+            $ram = $this->ramRepository->selectById($r->RamID);
+            if($ram->ModelName == $r->ModelName && $ram->Size == $r->Size)
                 throw new ServiceException("Ram name and size already used!");
 
-            $this->ramRepository->insert($s);
+            return $this->ramRepository->insert($r);
         }
 
+        /**
+         * Select ram by id
+         * @param int $id The id to select
+         * @return Ram The ram selected
+         * @throws ServiceException If not found
+         */
         public function selectById(int $id): Ram{
             $ram = $this->ramRepository->selectById($id); 
             if($ram == null) throw new ServiceException("Ram not found");
@@ -31,6 +44,12 @@
             return $ram;
         }
 
+        /**
+         * Select ram by name
+         * @param string $name The name to select
+         * @return Ram The Ram selected
+         * @throws ServiceException If not found
+         */
         public function selectByName(string $name): Ram{
             $ram = $this->ramRepository->selectByName($name);
             if($ram == null) throw new ServiceException("Ram not found");
@@ -38,18 +57,34 @@
             return $ram;
         }
 
-        public function update(Ram $s):void{
-            if($this->ramRepository->selectById($s->RamID) == null)
+        /**
+         * Update a ram
+         * @param Ram $r The ram to update
+         * @return Ram The ram updated
+         * @throws ServiceException If not found
+         * @throws RepositoryException If the update fails
+         */
+        public function update(Ram $r):Ram{
+            if($this->ramRepository->selectById($r->RamID) == null)
                 throw new ServiceException("Ram not found!");
 
-            $this->ramRepository->update($s);
+            return $this->ramRepository->update($r);
         }
 
-        public function delete(int $id): void{
-            if($this->ramRepository->selectById($id) == null)
+        /**
+         * Delete ram
+         * @param int $id The id to delete
+         * @return Ram The ram deleted
+         * @throws ServiceException If not found
+         * @throws RepositoryException If the delete fails
+         */
+        public function delete(int $id): Ram{
+            $r = $this->ramRepository->selectById($id);
+            if($r == null)
                 throw new ServiceException("Ram not found!");
 
             $this->ramRepository->delete($id);
+            return $r;
         }
     }
 
