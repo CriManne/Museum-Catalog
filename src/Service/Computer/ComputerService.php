bd060e8a790360fb985274c5a911cc38326db948
Cristian Mannella <cristianmannellapc@gmail.com>
2022-08-26 11:51:49 +0200

    Computer Service and unit tests

---
+++ src/Service/Computer/ComputerService.php
@@ -0,0 +1,90 @@
+<?php
+    declare(strict_types=1);
+
+    namespace App\Service\Computer;
+
+    use App\Exception\ServiceException;
+    use App\Model\Computer\Computer;
+    use App\Repository\Computer\ComputerRepository;
+
+    class ComputerService{
+
+        public ComputerRepository $computerRepository;
+
+        public function __construct(ComputerRepository $computerRepository)
+        {
+            $this->computerRepository = $computerRepository;
+        }
+
+        /**
+         * Insert computer
+         * @param Computer $c The computer to insert
+         * @return Computer The computer inserted
+         * @throws ServiceException If the ModelName is already used
+         * @throws RepositoryException If the insert fails
+         */
+        public function insert(Computer $c):Computer{
+            if($this->computerRepository->selectByModelName($c->ModelName) != null)
+                throw new ServiceException("Computer already used!");
+
+            return $this->computerRepository->insert($c);
+        }
+
+        /**
+         * Select by id
+         * @param string $id The id to select
+         * @return Computer The computer selected
+         * @throws ServiceException If not found
+         */
+        public function selectById(string $id): Computer{
+            $computer = $this->computerRepository->selectById($id); 
+            if($computer == null) throw new ServiceException("Computer not found");
+
+            return $computer;
+        }
+
+        /**
+         * Select by ModelName
+         * @param string $ModelName The ModelName to select
+         * @return Computer The computer selected
+         * @throws ServiceException If not found
+         */
+        public function selectByModelName(string $ModelName): Computer{
+            $computer = $this->computerRepository->selectByModelName($ModelName);
+            if($computer == null) throw new ServiceException("Computer not found");
+
+            return $computer;
+        }
+
+        /**
+         * Update a Computer
+         * @param Computer $c The Computer to update
+         * @return Computer The computer updated
+         * @throws ServiceException If not found
+         * @throws RepositoryException If the update fails
+         */
+        public function update(Computer $c):Computer{
+            if($this->computerRepository->selectById($c->ObjectID) == null)
+                throw new ServiceException("Computer not found!");
+
+            return $this->computerRepository->update($c);
+        }
+
+        /**
+         * Delete a Computer
+         * @param string $id The id to delete
+         * @return Computer The computer deleted
+         * @throws ServiceException If not found
+         * @throws RepositoryException If the delete fails
+         */
+        public function delete(string $id): Computer{
+            $c = $this->computerRepository->selectById($id);
+            if($c == null)
+                throw new ServiceException("Computer not found!");
+
+            $this->computerRepository->delete($id);
+            return $c;
+        }
+    }
+
+?>
---
+++ tests/Service/Computer/ComputerServiceTest.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Exception\ServiceException;
+
+use App\Model\Computer\Os;
+use App\Model\Computer\Ram;
+use App\Model\Computer\Cpu;
+use App\Model\Computer\Computer;
+
+use App\Repository\Computer\ComputerRepository;
+use App\Service\Computer\ComputerService;
+
+final class ComputerServiceTest extends TestCase
+{
+    public ComputerRepository $computerRepository;
+    public ComputerService $computerService;
+    
+
+    public function setUp(): void
+    {                
+        $this->computerRepository = $this->createMock(ComputerRepository::class);
+
+        $this->computerService = new ComputerService($this->computerRepository);        
+
+        $this->sampleObject = new Computer(
+            "objID",
+            null,
+            null,
+            null,
+            "1",
+            null,
+            'Computer 1',
+            2005,
+            "1TB",            
+            new Cpu(1, 'Cpu 1.0','2GHZ'),
+            new Ram(1, 'Ram 1.0','64GB'),
+            new Os(1, 'Windows'),
+        );    
+        
+        $this->sampleObjectRaw = [
+            'ModelName' => 'Computer 1',
+            'Year' => 2005,
+            'HDDSize' => "1TB",
+            'OsID' => 1,
+            'RamID' => 1,
+            'CpuID' => 1,
+            'ObjectID' => 'objID',
+            'Note' => null,
+            'Url' => null,
+            'Tag' => null,
+            'Active' => '1',
+            'Erased' => null,
+        ];        
+    }
+    
+    
+    //INSERT TESTS
+    
+    public function testBadInsert():void{
+        $this->expectException(ServiceException::class);
+        $this->computerRepository->method('selectByModelName')->willReturn($this->sampleObject);
+        $this->computerService->insert($this->sampleObject);
+    }
+    //SELECT TESTS
+    public function testGoodSelectById(): void
+    {
+        $this->computerRepository->method('selectById')->willReturn($this->sampleObject);
+        $this->assertEquals("Computer 1",$this->computerService->selectById("ObjID")->ModelName);
+    }
+    
+    public function testBadSelectById(): void
+    {
+        $this->expectException(ServiceException::class);
+        $this->computerRepository->method('selectById')->willReturn(null);
+        $this->computerService->selectById("ObjID25");
+    }
+    
+    public function testBadSelectByName(): void
+    {
+        $this->expectException(ServiceException::class);
+        $this->computerRepository->method('selectByModelName')->willReturn(null);
+        $this->computerService->selectByModelName("WRONG");
+    }
+    
+    //UPDATE TESTS
+    public function testBadUpdate():void{
+        $this->expectException(ServiceException::class);                
+        $this->computerRepository->method('selectById')->willReturn(null);
+        $this->computerService->update($this->sampleObject);
+    }
+    
+    //DELETE TESTS
+    public function testBadDelete():void{
+        $this->expectException(ServiceException::class);
+        
+        $this->computerRepository->method('selectById')->willReturn(null);
+        
+        $this->computerService->delete("ObjID99");
+    }       
+}
