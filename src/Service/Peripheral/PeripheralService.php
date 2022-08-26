24dbb8b98740ef30d37edbb4ca25c1979d1051b0
Cristian Mannella <cristianmannellapc@gmail.com>
2022-08-26 12:02:51 +0200

    PeripheralService and unit tests

---
+++ src/Service/Peripheral/PeripheralService.php
@@ -0,0 +1,90 @@
+<?php
+    declare(strict_types=1);
+
+    namespace App\Service\Peripheral;
+
+    use App\Exception\ServiceException;
+    use App\Model\Peripheral\Peripheral;
+    use App\Repository\Peripheral\PeripheralRepository;
+
+    class PeripheralService{
+
+        public PeripheralRepository $peripheralRepository;
+
+        public function __construct(PeripheralRepository $peripheralRepository)
+        {
+            $this->peripheralRepository = $peripheralRepository;
+        }
+
+        /**
+         * Insert peripheral
+         * @param Peripheral $p The peripheral to insert
+         * @return Peripheral The peripheral inserted
+         * @throws ServiceException If the ModelName is already used
+         * @throws RepositoryException If the insert fails
+         */
+        public function insert(Peripheral $p):Peripheral{
+            if($this->peripheralRepository->selectByModelName($p->ModelName) != null)
+                throw new ServiceException("Peripheral already used!");
+
+            return $this->peripheralRepository->insert($p);
+        }
+
+        /**
+         * Select by id
+         * @param string $id The id to select
+         * @return Peripheral The peripheral selected
+         * @throws ServiceException If not found
+         */
+        public function selectById(string $id): Peripheral{
+            $peripheral = $this->peripheralRepository->selectById($id); 
+            if($peripheral == null) throw new ServiceException("Peripheral not found");
+
+            return $peripheral;
+        }
+
+        /**
+         * Select by ModelName
+         * @param string $ModelName The ModelName to select
+         * @return Peripheral The peripheral selected
+         * @throws ServiceException If not found
+         */
+        public function selectByModelName(string $ModelName): Peripheral{
+            $peripheral = $this->peripheralRepository->selectByModelName($ModelName);
+            if($peripheral == null) throw new ServiceException("Peripheral not found");
+
+            return $peripheral;
+        }
+
+        /**
+         * Update a Peripheral
+         * @param Peripheral $p The Peripheral to update
+         * @return Peripheral The peripheral updated
+         * @throws ServiceException If not found
+         * @throws RepositoryException If the update fails
+         */
+        public function update(Peripheral $p):Peripheral{
+            if($this->peripheralRepository->selectById($p->ObjectID) == null)
+                throw new ServiceException("Peripheral not found!");
+
+            return $this->peripheralRepository->update($p);
+        }
+
+        /**
+         * Delete a Peripheral
+         * @param string $id The id to delete
+         * @return Peripheral The peripheral deleted
+         * @throws ServiceException If not found
+         * @throws RepositoryException If the delete fails
+         */
+        public function delete(string $id): Peripheral{
+            $p = $this->peripheralRepository->selectById($id);
+            if($p == null)
+                throw new ServiceException("Peripheral not found!");
+
+            $this->peripheralRepository->delete($id);
+            return $p;
+        }
+    }
+
+?>
---
+++ tests/Service/Peripheral/PeripheralServiceTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Service;
+
+use PHPUnit\Framework\TestCase;
+use App\Exception\ServiceException;
+
+use App\Model\Peripheral\Peripheral;
+use App\Model\Peripheral\PeripheralType;
+
+use App\Repository\Peripheral\PeripheralRepository;
+use App\Service\Peripheral\PeripheralService;
+
+final class PeripheralServiceTest extends TestCase
+{
+    public PeripheralRepository $peripheralRepository;
+    public PeripheralService $peripheralService;
+    
+
+    public function setUp(): void
+    {                
+        $this->peripheralRepository = $this->createMock(PeripheralRepository::class);
+
+        $this->peripheralService = new PeripheralService($this->peripheralRepository);        
+
+        $this->sampleObject = new Peripheral(
+            "objID",
+            null,
+            null,
+            null,
+            "1",
+            null,
+            'Peripheral 1.0',
+            new PeripheralType(1, 'PeripheralType 1')
+        );    
+        
+        $this->sampleObjectRaw = [
+            'ModelName' => 'Peripheral 1.0',
+            'PeripheralTypeID' => 1,
+            'ObjectID' => 'objID',
+            'Note' => null,
+            'Url' => null,
+            'Tag' => null,
+            'Active' => '1',
+            'Erased' => null,
+        ];        
+    }
+    
+    
+    //INSERT TESTS
+    
+    public function testBadInsert():void{
+        $this->expectException(ServiceException::class);
+        $this->peripheralRepository->method('selectByModelName')->willReturn($this->sampleObject);
+        $this->peripheralService->insert($this->sampleObject);
+    }
+    //SELECT TESTS
+    public function testGoodSelectById(): void
+    {
+        $this->peripheralRepository->method('selectById')->willReturn($this->sampleObject);
+        $this->assertEquals("Peripheral 1.0",$this->peripheralService->selectById("ObjID")->ModelName);
+    }
+    
+    public function testBadSelectById(): void
+    {
+        $this->expectException(ServiceException::class);
+        $this->peripheralRepository->method('selectById')->willReturn(null);
+        $this->peripheralService->selectById("ObjID25");
+    }
+    
+    public function testBadSelectByName(): void
+    {
+        $this->expectException(ServiceException::class);
+        $this->peripheralRepository->method('selectByModelName')->willReturn(null);
+        $this->peripheralService->selectByModelName("WRONG");
+    }
+    
+    //UPDATE TESTS
+    public function testBadUpdate():void{
+        $this->expectException(ServiceException::class);                
+        $this->peripheralRepository->method('selectById')->willReturn(null);
+        $this->peripheralService->update($this->sampleObject);
+    }
+    
+    //DELETE TESTS
+    public function testBadDelete():void{
+        $this->expectException(ServiceException::class);
+        
+        $this->peripheralRepository->method('selectById')->willReturn(null);
+        
+        $this->peripheralService->delete("ObjID99");
+    }       
+}
