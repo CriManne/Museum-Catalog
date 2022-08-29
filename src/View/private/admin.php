3f3116e55f885db6e9482d85640befc5acd75cd8
Cristian Mannella <cristianmannellapc@gmail.com>
2022-08-29 13:47:04 +0200

    Update admin.php

--- src/View/private/admin.php
+++ src/View/private/admin.php
@@ -14,11 +14,17 @@ use App\Util\DIC;
     try{
         $users = $userService->selectAll();
 
-        echo "<table><thead><tr><td>Nome</td><td>Cognome</td><td>Email</td><td>Privilegi</td></tr></thead><tbody>";
+        echo "<table class='table'><thead><tr>
+        <th scope='col'>Email</th>
+        <th scope='col'>Nome</th>
+        <th scope='col'>Cognome</th>
+        <th scope='col'>Privilegi</th>
+        <th scope='col'>Operazioni</th>
+        </tr></thead><tbody>";
 
 
         foreach($users as $user){
-            echo "<tr><td>$user->firstname</td><td>$user->lastname</td><td>$user->Email</td><td>$user->Privilege</td></tr>";
+            echo "<tr><th scope='row'>$user->Email</th><td>$user->firstname</td><td>$user->lastname</td><td>$user->Privilege</td></tr>";
         }
         echo "</tbody></table>";
     }catch(ServiceException $e){
