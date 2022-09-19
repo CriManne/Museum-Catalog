bf77a9c5361883c71b9309b81d4f67a31132b16e
Cristian Mannella <cristianmannellapc@gmail.com>
2022-09-19 19:22:38 +0200

    View artifacts file

--- config/container.php
+++ config/container.php
@@ -21,6 +21,7 @@ return [
         $engine->addFolder('layouts', $c->get('view_path') . '/layouts');
         $engine->addFolder('private', $c->get('view_path') . '/private');
         $engine->addFolder('p_admin', $c->get('view_path') . '/private/admin');
+        $engine->addFolder('reusable', $c->get('view_path') . '/reusable');
         $engine->addFolder('public', $c->get('view_path') . '/public');
         $engine->addFolder('artifact', $c->get('view_path') . '/public/artifact');
         $engine->addFolder('error', $c->get('view_path') . '/error');
---
+++ src/View/reusable/viewArtifacts.php
@@ -0,0 +1,50 @@
+<style>
+    .table-scrollable {
+        overflow-x: auto;
+        max-width: 100vw;
+        box-shadow: inset 0 0 5px rgba(150, 150, 150, 0.35);
+        margin: auto;
+        padding: 0;
+        padding-top: 10px;
+    }
+</style>
+<h3>Visualizza reperti</h3>
+<form id='artifact-search-form'>
+
+    <div class="input-group mb-3">
+        <select id="template" style="display:none;">
+            <option id="templateOption"></option>
+        </select>
+        <!-- this is our "real" template that will be re-sized -->
+        <select class="input-select" id="category-select">
+            <option selected value="">Tutte le categorie</option>            
+        </select>
+        <input type="text" class="form-control" placeholder="Search artifact" aria-label="Artifact's search" id="artifact-search">
+        <input type='submit' class='btn btn-primary'>
+    </div>
+</form>
+
+<div class="container-fluid d-flex flex-column p-0 gap-2 align-items-center" id="result-container">
+    <div class="container-fluid table-scrollable" id="tb-container"></div>
+</div>
+<div class="alert alert-danger" role="alert" id="error-alert"></div>
+
+<?php $this->push('scripts') ?>
+<script src="/resources/js/util.js"></script>
+<script src="/resources/js/artifacts.js"></script>
+<script>
+function setSelectWidth() {
+    var sel = $('#sel');
+    $('#templateOption').text( sel.val() );
+    // for some reason, a small fudge factor is needed
+    // so that the text doesn't become clipped
+    sel.width( $('#template').width() * 1.03 );
+}
+
+setSelectWidth();
+
+$('#sel').change( function() {
+    setSelectWidth();
+} );
+</script>
+<?php $this->end() ?>
